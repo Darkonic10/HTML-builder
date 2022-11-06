4695fd5b9d528d1e5bc3f2a9dcc585569aa8668d
Darkonic10 <tiynov2@mail.ru>
2022-11-06 19:24:03 +0500

    fix: minor change kb to bits for cross-check

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,7 +8,7 @@ fs.readdir(folder, {withFileTypes: true}, (err, files) => {
     if(!file.isDirectory()) {
       const fileName = file.name.replace(/\..*/, '');
       const fileExtName = path.extname(file.name).slice(1);
-      fs.stat(path.resolve(__dirname, 'secret-folder', `${file.name}`), (err, stats) => console.log(`${fileName} - ${fileExtName} - ${stats.size / 1024 + 'kb'}`))
+      fs.stat(path.resolve(__dirname, 'secret-folder', `${file.name}`), (err, stats) => console.log(`${fileName} - ${fileExtName} - ${stats.size + 'b'}`))
     }
   });
 });
